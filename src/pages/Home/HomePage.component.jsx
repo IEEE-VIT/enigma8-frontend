64823dcb39f39f907d1bf5d034efed116e6200ea
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-09-23 08:22:03 +0530

    fix: unwanted call

--- src/pages/Home/HomePage.component.jsx
+++ src/pages/Home/HomePage.component.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect } from "react";
-import { testRoute } from "../../api/auth";
 
 const HomePage = () => {
-  useEffect(() => {
-    testRoute();
-  }, []);
   return <div>Home</div>;
 };
 
