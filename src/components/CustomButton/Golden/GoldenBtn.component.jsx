f5dee23e374c16426f85b3044bf8c0f0229f6f74
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-11-14 23:57:05 +0530

    add: countdown page [ui] (#54)

--- src/components/CustomButton/Golden/GoldenBtn.component.jsx
+++ src/components/CustomButton/Golden/GoldenBtn.component.jsx
@@ -1,23 +1,30 @@
-// import React from "react";
-// import "./GoldenBtn.styles.css";
+import React from "react";
+import "./GoldenBtn.styles.css";
 
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-// const GoldenBtn = ({ children, triggerFunction }) => {
-//   const handleClick = () => {
-//     triggerFunction();
-//   };
-//   console.log(children);
-//   return (
-//     <div>
-//       <button type="submit" onClick={handleClick} className="golden-btn">
-//         {children}
-//       </button>
-//     </div>
-//   );
-// };
-// GoldenBtn.propTypes = {
-//   children: PropTypes.string.isRequired,
-//   triggerFunction: PropTypes.func.isRequired,
-// };
-// export default GoldenBtn;
+const GoldenBtn = (props) => {
+  const { children, triggerFunction, width, marginTop } = props;
+  const handleClick = () => {
+    triggerFunction();
+  };
+  return (
+    <div>
+      <button
+        type="submit"
+        style={{ width, marginTop }}
+        onClick={handleClick}
+        className="golden-btn"
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+GoldenBtn.propTypes = {
+  children: PropTypes.string.isRequired,
+  triggerFunction: PropTypes.func.isRequired,
+  width: PropTypes.string.isRequired,
+  marginTop: PropTypes.string.isRequired,
+};
+export default GoldenBtn;
