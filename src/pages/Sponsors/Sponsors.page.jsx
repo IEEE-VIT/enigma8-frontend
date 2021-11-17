20da5d8c0b3d1b3ddc522a3fdf484a3c72769a92
Akshaya Chelliah <akshayav7c@gmail.com>
2021-11-17 18:34:12 +0530

    fix: update manifest and sponsors #52 (#76)
    * add: FAQ Page [ui] #53
    
    * add: Sponsors Page [ui] #52
    
    * fix: update manifest, sponsors #52
    
    * fix: bugs

--- src/pages/Sponsors/Sponsors.page.jsx
+++ src/pages/Sponsors/Sponsors.page.jsx
@@ -6,11 +6,18 @@ import SponsorsCard from "../../components/SponsorsCard/SponsorsCard.component";
 import BackgroundLeft from "../../assets/home/bg-left.svg";
 import BackgroundRight from "../../assets/home/bg-right.svg";
 
-import TitleSponsor from "../../assets/home/title-sponsor.svg";
-import PowerdBySponsor from "../../assets/home/powered-by.svg";
-// import DigitalPartner from "../../assets/home/digital-partner.svg";
+import TitleSponsor from "../../assets/home/sponsor-titlesponsor.svg";
+import PowerdBySponsor from "../../assets/home/sponsor-poweredby.svg";
+import GiftingPartner from "../../assets/home/sponsor-gifting-partner.svg";
 // import IPRPartner from "../../assets/home/ipr-partner.svg";
-import BlockchainPartner from "../../assets/home/blockchain-partner.svg";
+import BlockchainPartner from "../../assets/home/sponsor-blockchain-partner.svg";
+import PartnerA from "../../assets/home/sponsor-partner-a.svg";
+import PartnerB from "../../assets/home/sponsor-partner-b.svg";
+import PartnerC from "../../assets/home/sponsor-partner-c.svg";
+
+import DigitalPartnerA from "../../assets/home/sponsor-digital-partner-a.svg";
+import DigitalPartnerB from "../../assets/home/sponsor-digital-partner-b.svg";
+
 // import EmptyTile from "../../assets/home/sponsor-empty-tile.svg";
 import FooterNav from "../../components/FooterNav/FooterNav.component";
 
@@ -38,26 +45,56 @@ const Sponsors = () => {
             title="BLOCKCHAIN PARTNERS"
             link="https://dhiway.com/"
             type={3}
+          />{" "}
+          <SponsorsCard
+            img={GiftingPartner}
+            title="GIFTING PARTNERS"
+            link="https://offostore.com/"
+            type={3}
           />
           {/* <SponsorsCard
             img={EmptyTile}
             title="CONNECTION PARTNERS"
             link=""
             type={3}
-          />
-          <SponsorsCard
-            img={EmptyTile}
-            title="GIFTING PARTNERS"
-            link=""
-            type={3}
           /> */}
         </div>
-        {/* <SponsorsCard img={" "} title="PARTNERS" link="" type={4} /> */}
-        {/* <div className="sponsorcard-bag">
-          <SponsorsCard img={EmptyTile} title="" link="" type={4} />
-          <SponsorsCard img={EmptyTile} title="" link="" type={4} />
-          <SponsorsCard img={EmptyTile} title="" link="" type={4} />
-        </div> */}
+        <SponsorsCard img={" "} title="PARTNERS" link="" type={0} />
+        <div className="sponsorcard-bag">
+          <SponsorsCard
+            img={PartnerA}
+            title=""
+            link="https://gen.xyz/"
+            type={3}
+          />
+          <SponsorsCard
+            img={PartnerB}
+            title=""
+            link="https://bit.ly/3aLF5vB"
+            type={3}
+          />
+          <SponsorsCard
+            img={PartnerC}
+            title=""
+            link="https://replit.com/"
+            type={3}
+          />
+        </div>
+        <SponsorsCard img={" "} title="DIGITAL PARTNERS" link="" type={0} />
+        <div className="sponsorcard-bag">
+          <SponsorsCard
+            img={DigitalPartnerA}
+            title=""
+            link="https://collegeinsider.in/"
+            type={4}
+          />
+          <SponsorsCard
+            img={DigitalPartnerB}
+            title=""
+            link="https://moroesports.com/"
+            type={4}
+          />
+        </div>
       </div>
       <img src={BackgroundLeft} className="home-bg-left" alt="" />
       <img src={BackgroundRight} className="home-bg-right" alt="" />
