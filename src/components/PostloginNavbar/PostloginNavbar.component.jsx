8dd0568f9f334d6b261e741a4faa54dc54c956de
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-11-24 15:57:08 +0530

    add: Instructions Component [wireframe]

--- src/components/PostloginNavbar/PostloginNavbar.component.jsx
+++ src/components/PostloginNavbar/PostloginNavbar.component.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PostloginNavbar.styles.css";
 import { Link } from "react-router-dom";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import ProfileMenu from "../Menu/ProfileMenu/ProfileMenu.component";
+import Instructions from "../Instructions/Instructions.component";
 
 const PostloginNavbar = () => {
+  const [openInstructions, setOpenInstructions] = useState(false);
+  const handleClose = () => {
+    setOpenInstructions(false);
+  };
+  const handleOpen = () => {
+    setOpenInstructions(true);
+  };
   return (
     <nav style={{ display: "flex", justifyContent: "space-between" }}>
       <Link to="/">
@@ -17,7 +25,28 @@ const PostloginNavbar = () => {
         <li>
           <Link to="/story">Story</Link>
         </li>
-        <li>Instructions</li>
+        {/* <li>Instructions</li> */}
+        {openInstructions ? (
+          <Instructions
+            handleClose={handleClose}
+            openInstructions={openInstructions}
+          />
+        ) : (
+          <> </>
+        )}
+
+        <button
+          type="button"
+          style={{
+            background: "none",
+            border: "none",
+            margin: "0",
+            padding: "0",
+          }}
+          onClick={handleOpen}
+        >
+          Instructions
+        </button>
         <li>
           <Link to="/leaderboard">Leaderboard</Link>
         </li>
