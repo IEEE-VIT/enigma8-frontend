b2b0b5840ce79cd9e065b41d2785166579f582d7
Akshaya-vc <akshayav7c@gmail.com>
2021-11-25 03:28:15 +0530

    add: rooms page [ui] #56

--- src/pages/Rooms/Rooms.page.jsx
+++ src/pages/Rooms/Rooms.page.jsx
@@ -2,14 +2,22 @@ import React, { useState, useEffect } from "react";
 import Room from "../../components/Room/Room.component";
 import "./Rooms.styles.css";
 import { allRooms } from "../../api/room";
+import keys from "../../assets/profile/solved-key.svg";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [stars, setStars] = useState(0);
   useEffect(() => {
     allRooms()
       .then((res) => {
         for (let i = 0; i < res.data.data.data.length; i += 1) {
           setRooms((room) => [...room, res.data.data.data[i]]);
+        }
+        for (let i = 0; i < res.data.data.data.length; i += 1) {
+          if (res.data.data.data[i].starsLeft > 0) {
+            setStars(res.data.data.data[i].starsLeft);
+            break;
+          }
         }
       })
       .catch((err) => {
@@ -22,7 +30,15 @@ const Rooms = () => {
       <Room key={room.room.roomNo} room={room.room} journey={room.journey} />
     );
   });
-  return <div className="rooms-container">{roomslist}</div>;
+  return (
+    <div className="rooms-page">
+      <div className="rooms-status">
+        <div className="room-status-text">Next room unlocks in {stars}</div>
+        <img src={keys} alt="" />
+      </div>
+      <div className="rooms-container"> {roomslist}</div>
+    </div>
+  );
 };
 
 export default Rooms;
